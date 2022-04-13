21b31f4d76283d104020f9eb4da363adee15c422
jch1013 <93944737+jch1013@users.noreply.github.com>
2022-04-13 13:21:34 -0500

    Update main.py
    functional scraper

--- Research/Jch5/WebScraper/main.py
+++ Research/Jch5/WebScraper/main.py
@@ -10,38 +10,24 @@ html = bs4.BeautifulSoup(req.text, "html.parser")
 #This should grab all the the things in this class, which should be what we want
 content = html.select(".relatedthumb")
 
-#checking what we got...
-i = 5
-#print((content[i]))
-#print(content[0].text)
-#print(len(content))
-
-
 images = []
 names = []
 
-"""
-So i cant figure out how to get the image tags into the list, i tried to 
-isolate the tags with the loop below but for some reason about 20% of the 
-images arent being added...
-"""
-
-
 for item in content:
-    names.append(item.text)
-    itemstring = str(item)
-    itemlist = itemstring.split(" ")
-    itemlist = itemlist[10:20]
+    try:
+        pic = item.find('img')
+        images.append(pic['src'])
+        names.append(item.text)
+    except:
+        continue
 
-    for elem in itemlist:
-        part = elem.split('=')
-        if part[0] == "src" or part[0] == "srcset":
-            images.append(elem)
+print(len(names))
+print(len(images))
 
 
-
-print((names[i]))
-print((images[i]))
+i = 360
+print(names[i])
+print(images[i])
 
 
 
